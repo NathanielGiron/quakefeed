c032689ceea44ac4925cfa26215342494efd9440
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-08-30 23:50:12 -0700

    quake data displayed

--- index.js
+++ index.js
@@ -9,10 +9,21 @@ app.use(ejsLayouts);
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
-	res.render('index');
+  res.render('index');
+});
+
+app.get('/feed', function(req, res) {
+  request('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson', function (error, response, body) {
+  	
+  	var dataObj = JSON.parse(body);
+  	console.log(dataObj);
+    if (!error && response.statusCode === 200) {
+      res.render('feed', {feed: dataObj});
+    }
+  });
 });
 
 var port = 3000;
 app.listen(port, function() {
-	console.log("You're listening to the smooth sounds of port " + port);
+  console.log("You're listening to the smooth sounds of port " + port);
 });
